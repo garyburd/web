ec5ddd2d44c8788b32f31989410187b91ed92a90
Gary Burd <gary@beagledreams.com>
2019-02-12 08:31:52 -0800

    templates: add function to create from struct fields

--- templates/template.go
+++ templates/template.go
@@ -10,13 +10,14 @@ import (
 	"net/http"
 	"path"
 	"path/filepath"
+	"reflect"
 	"strings"
 	ttemp "text/template"
 )
 
 type Template struct {
 	fileNames       []string
-	contentType     string
+	mimeType        string
 	execute         func(w io.Writer, v interface{}) error
 	executeTemplate func(w io.Writer, name string, v interface{}) error
 	hasTemplate     func(name string) bool
@@ -39,7 +40,7 @@ func (t *Template) WriteResponse(w http.ResponseWriter, r *http.Request, statusC
 	if err := t.execute(&buf, data); err != nil {
 		return err
 	}
-	w.Header().Set("Content-Type", t.contentType)
+	w.Header().Set("Content-Type", t.mimeType)
 
 	if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 		w.WriteHeader(statusCode)
@@ -68,7 +69,7 @@ type Manager struct {
 // files in common suffixes across templates are parsed once. File names are
 // relative to the directory name passed to Load.
 func (m *Manager) NewHTML(fileNames ...string) *Template {
-	t := &Template{fileNames: fileNames, contentType: mime.TypeByExtension(path.Ext(fileNames[0]))}
+	t := &Template{fileNames: fileNames, mimeType: mime.TypeByExtension(path.Ext(fileNames[0]))}
 	m.htmlTemplates = append(m.htmlTemplates, t)
 	return t
 }
@@ -78,9 +79,49 @@ func (m *Manager) NewHTML(fileNames ...string) *Template {
 // suffixes across templates are parsed once. File names are relative to the
 // directory name passed to Load.
 func (m *Manager) NewText(fileNames ...string) *Template {
-	t := &Template{fileNames: fileNames, contentType: mime.TypeByExtension(path.Ext(fileNames[0]))}
+	t := &Template{fileNames: fileNames, mimeType: mime.TypeByExtension(path.Ext(fileNames[0]))}
 	m.textTemplates = append(m.textTemplates, t)
 	return t
+}
+
+var templatePtrType = reflect.TypeOf((*Template)(nil))
+var tagfns = []struct {
+	tag string
+	fn  func(*Manager, ...string) *Template
+}{{"html", (*Manager).NewHTML}, {"text", (*Manager).NewText}}
+
+// NewFromFields creates templates for fields in the struct pointed to by sp
+// with text or html field tags. The value of the tag is a space separated list
+// of the template files.
+func (m *Manager) NewFromFields(sp interface{}) {
+	const mustBePointerToStruct = "NewFromFields argument must be a pointer to a struct"
+
+	v := reflect.ValueOf(sp)
+	if v.Kind() != reflect.Ptr {
+		panic(mustBePointerToStruct)
+	}
+	v = v.Elem()
+	if v.Kind() != reflect.Struct {
+		panic(mustBePointerToStruct)
+	}
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		ft := t.Field(i)
+		if ft.Type != templatePtrType {
+			continue
+		}
+		for _, tagfn := range tagfns {
+			value := ft.Tag.Get(tagfn.tag)
+			if value != "" {
+				if ft.PkgPath != "" {
+					panic(fmt.Sprintf("fields with %s tag must be exported", tagfn.tag))
+				}
+				f := v.Field(i)
+				f.Set(reflect.ValueOf(tagfn.fn(m, strings.Fields(value)...)))
+				break
+			}
+		}
+	}
 }
 
 // Load loads the templates from the specified directory.
